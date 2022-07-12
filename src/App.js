ebd3263d0e8b0fac1a63da1ca9a6bcb0ab83d301
saida burmaganova <saida.burmaganova@gmail.com>
2022-07-12 18:32:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="first">
+      <h1 className="header"> Отбасы құр</h1>
+     <img src="Group 172.jpg" alt="rings" className='image'/>
     </div>
   );
 }
