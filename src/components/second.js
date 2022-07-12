9c7aeab8606ee272a17365c537a235d2cbd94910
saida burmaganova <saida.burmaganova@gmail.com>
2022-07-13 03:15:07 +0600

    git chekout

--- src/components/second.js
+++ src/components/second.js
@@ -7,16 +7,11 @@ const Second = () => {
         <h1 className="header3"> СУРЕТКЕ ТҮС </h1>
         <h2 className="header2"> бірінші адам </h2>
         <form>
-  <label> Атыңыз: </label>
-  <input 
-  type = "text"
-  required/>
-    <label> Тегіңіз: </label>
-    <input 
-    type="text"
-    required
-    />
-    <button> Келесі</button>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
 </form>
            </div>
            
