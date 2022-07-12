b73fa3c8fac8deaf5990661058b411b11a104865
saida burmaganova <saida.burmaganova@gmail.com>
2022-07-13 01:20:29 +0600

    new

--- src/components/second.js
+++ src/components/second.js
@@ -4,9 +4,15 @@ import '../App.css';
 const Second = () => {
     return (
            <div className="first">
-        <h1 className="header"> СУРЕТКЕ ТҮС </h1>
+        <h1 className="header3"> СУРЕТКЕ ТҮС </h1>
         <h2 className="header2"> бірінші адам </h2>
-       
+        <form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
            </div>
            
     )
