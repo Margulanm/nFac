f04e17c35e328b7df3e4f1dda415d555ee44f049
saida burmaganova <saida.burmaganova@gmail.com>
2022-07-12 23:57:12 +0600

    new

--- src/components/first.js
+++ src/components/first.js
@@ -6,16 +6,7 @@ const First = ({handleOne}) => {
         <div className="first">
         <h1 className="header"> Отбасы құр</h1>
        <img src="Group 172.jpg" alt="rings" className='image'/>
-<<<<<<< HEAD
-       <button className="buttonOne" onClick={()=>{handleOne();
-       console.log('hjvzgxsjdhca');
-    }}> Үйлену </button>
-=======
-       <div className="container">
-            <button className="buttonOne"> Үйлену </button>
-       </div>
-
->>>>>>> b267564702fc2499f930bd608718f60f08d53393
+       <button className="buttonOne" onClick={handleOne}> Үйлену </button>
       </div>
     )
 }
